8ab07d1fee467077a2e31e496bf1efe54ecabd42
USERNAME <EMAIL>
2023-01-15 14:12:57 -0500

    added a little readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # F1app
+
+This is my Formula One app.
+
+I have really ***enjoyed*** this sport, and have watched it from about 2000 to present.
+
+I hope you enjoy the App !
--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 const drivers = "http://ergast.com/api/f1/drivers/alonso.json"
 const constructors = "http://ergast.com/api/f1/constructors"
 const limit = "?limit = 1000"
-const driver = "{driver}"
+const driver = "(event)"
 const json1 = ".json"
 const DriversNames = []
+const questionButton = document.querySelector("button")
 
 fetch(drivers)
 .then((response) => response.json())
-.then((data) => console.log(data));
+// .then((data) => console.log(data));
+.then (function(response) {
+    if (response) {
+        const p = document.createElement("p");
+        p.innerText = "We are Connected to the ergast API";
+        document.body.appendChild(p); 
+    }
+})
 
-const p = document.createElement("p");
-p.innerText = "We are Connected to the ergast API";
-document.body.appendChild(p);
+questionButton.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    fetch(`${drivers}${driver}${json1}`)
+  .then((res) => res.json())
+    .then((response) => {
+        console.log(response.results)
+
+    })
 
