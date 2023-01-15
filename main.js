e17124aa996e148d38eef1381bfac5e9cb600624
USERNAME <EMAIL>
2023-01-15 11:33:58 -0500

    some styling

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-const drivers = "http://ergast.com/api/f1/drivers?limit=1000"
-const constructors = "http://ergast.com/api/f1/constructors?limit=1000"
+const drivers = "http://ergast.com/api/f1/drivers/alonso.json"
+const constructors = "http://ergast.com/api/f1/constructors"
 const limit = "?limit = 1000"
+const driver = "{driver}"
+const json1 = ".json"
+const DriversNames = []
 
+fetch(drivers)
+.then((response) => response.json())
+.then((data) => console.log(data));
 
+const p = document.createElement("p");
+p.innerText = "We are Connected to the ergast API";
+document.body.appendChild(p);
 
-
-document.querySelector("form").addEventListener("submit", (event) => {
-    event.preventDefault()})
